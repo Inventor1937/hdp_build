1faff8ca8226d4e264cff324fff69e5e494d2286
Miguel Martinez <slverwolf@gmail.com>
2018-01-27 20:26:46 -0600

    core/combo: Add basic armv7-a arch flags and set as default
    Change-Id: Idb4ffb096e461e48e35565bad0e2da0355c1ebec

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -5,3 +5,6 @@ ARCH_ARM_HAVE_ARMV7A            := true
 ARCH_ARM_HAVE_VFP               := true
 ARCH_ARM_HAVE_VFP_D32           := true
 ARCH_ARM_HAVE_NEON              := true
+arch_variant_cflags += \
+    -mfloat-abi=softfp \
+    -mfpu=neon
